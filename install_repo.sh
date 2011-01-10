3cde977933a3864af476e1d978bc409d78e3234e
wolfgang <wolfgang@b518e668-9675-4632-9382-9995c954fde7>
2011-01-10 03:43:06 +0000

    changed artifact name "com.tambur" to "net.tambur"
    git-svn-id: http://192.168.10.52/repos/dsn/mms/trunk@1675 b518e668-9675-4632-9382-9995c954fde7

--- install_repo.sh
+++ install_repo.sh
@@ -1,2 +1,2 @@
-mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile=lib/tambur-mmslib.jar -DgroupId=com.tambur -DartifactId=mmslib -Dversion=0.1 -Dpackaging=jar
+mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile=lib/tambur-mmslib.jar -DgroupId=net.tambur -DartifactId=mmslib -Dversion=0.1 -Dpackaging=jar
 
